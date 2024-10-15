c81714b7a71e7a35e32cfdd2bbaf3239c912b775
Franck Andriano <franck.andriano@webfg.com>
2024-10-15 21:22:26 +0200

    Clean code

--- src/main/java/com/jservlet/nexus/shared/web/controller/api/ApiBackend.java
+++ src/main/java/com/jservlet/nexus/shared/web/controller/api/ApiBackend.java
@@ -49,7 +49,6 @@ import org.springframework.web.servlet.HandlerMapping;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
